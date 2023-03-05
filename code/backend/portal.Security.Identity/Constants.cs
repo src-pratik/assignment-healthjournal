6ce2d64bca2c3598403a3c1095104f07d88159d9
Pratik Patil <connect@pratikpatil.net>
2023-03-05 12:50:29 +0530

    Added constants for roles

---
+++ code/backend/portal.Security.Identity/Constants.cs
@@ -0,0 +1,11 @@
+﻿namespace portal.Security.Identity
+{
+    public static class Constants
+    {
+        public static class Roles
+        {
+            public static string User = "User";
+            public static string Publisher = "Publisher";
+        }
+    }
+}
